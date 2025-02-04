67f63b298f405cafd1c93a891effd6d957e4116e
SEONG GEUN KIM <halo.sgkim@gmail.com>
2025-02-04 18:34:57 +0900

    Update DBClient.py
    - db 저장 경로 변경

--- DBClient.py
+++ DBClient.py
@@ -3,7 +3,7 @@ from langchain.vectorstores import Chroma
 
 class DBClient():
     def __init__(self, 
-                 persist_directory:str = '../resources/db/Galaxy25',
+                 persist_directory:str = './db/Galaxy25',
                  embedding_model="text-embedding-ada-002"):
         self.__embeddings = OpenAIEmbeddings( #← OpenAIEmbeddings를 초기화
             model=embedding_model
